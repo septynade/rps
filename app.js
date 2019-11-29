b2634613353cf55833ebc0a59c513f7e87f4216d
Ade <48345173+septynade@users.noreply.github.com>
2019-11-29 07:54:21 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -14,41 +14,62 @@ function getComputerChoice() {
     return choices[randomNumber];
 }
 
+function convertToWord(letter) {
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    return "Scissors";
+}
+
+function win(userChoice, computerChoice) {
+    const userChoice_div = document.getElementById(userChoice);
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(computerChoice)}. You Win! 👍🏻 `
+    userChoice_div.classList.add('green-glow');
+    setTimeout(() => userChoice_div.classList.remove('green-glow'), 500);
+}
+function lose(userChoice, computerChoice) {
+    const userChoice_div = document.getElementById(userChoice);
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `${convertToWord(userChoice)} loses to ${convertToWord(computerChoice)}. You Lost! 👎🏻 `
+    userChoice_div.classList.add('red-glow');
+    setTimeout(() => userChoice_div.classList.remove('red-glow'), 500);
+}
+function draw(userChoice, computerChoice) {
+    const userChoice_div = document.getElementById(userChoice);
+    result_p.innerHTML = `${convertToWord(userChoice)} equals to ${convertToWord(computerChoice)}. It's a Draw! 🤷🏻‍ `
+    userChoice_div.classList.add('grey-glow');
+    setTimeout(() => userChoice_div.classList.remove('grey-glow'), 500);
+}
+
 function game(userChoice) {
     const computerChoice = getComputerChoice();
     switch (userChoice + computerChoice) {
         case "rs":
         case "pr":
         case "sp":
-        console.log("User wins.");
-        break;
+            win(userChoice, computerChoice);
+            break;
         case "rp":
         case "ps":
         case "sr":
-        console.log("User loses.");
-        break;
+            lose(userChoice, computerChoice);
+            break;
         case "rr":
         case "pp":
-        case "ss":
-
-        
-        console.log("It's a draw");
-        break;
+        case "ss":      
+            draw(userChoice, computerChoice);
+            break;
     }
 }
 
 function main () {
-    rock_div.addEventListener('click', function(){
-        game("r");
-    })
-
-    paper_div.addEventListener('click', function(){
-        game("p");
-    })
-
-    scissors_div.addEventListener('click', function(){
-        game("s");
-    })
+    rock_div.addEventListener('click', () => game("r"));
+    paper_div.addEventListener('click', () => game("p"));
+    scissors_div.addEventListener('click', () => game("s"));
 }
 
 main();
